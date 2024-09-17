e3a87a94d842b2debd9a42c540f89e64bb12c0f3
mamancha <114275135+mamancha@users.noreply.github.com>
2024-09-16 21:11:34 -0500

    Update Hide.h

--- Hide.h
+++ Hide.h
@@ -4,7 +4,7 @@
 #include "Tools.h"
 
 void HideProcedure(char *messageFileName, char *coverFileName, char *outputFileName, int bits);
-void LoadPaletteHeader();
+void LoadPaletteHeader(int bits);
 void CoverLimitCheck(int bits);
 void EmbedLengthOfFile(int bitsToHide);
 void FreeHide();
